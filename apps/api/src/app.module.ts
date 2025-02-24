c73f20b2bed048c06cd2f81b8ac6b316b0c2587f
Wevioo <abderrahmenmhemed@192.168.1.9>
2025-02-24 11:40:46 +0100

    chore: db setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './projects/entities/project.entity';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'uat_user',
+      password: 'uat_password',
+      database: 'uat-db',
+      entities: [Project],
+      synchronize: true,
+    }),
+    ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ apps/api/src/projects/entities/project.entity.ts
@@ -0,0 +1,12 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Project {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name:string;
+
+    @Column()
+    description: string;
+}
