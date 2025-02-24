4528399de87cbcbc4ed79e67a49d48e2b9419ace
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2025-02-24 16:14:23 +0000

    chore: big update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,27 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@workspace/ui/components/button";
+import { Form } from "@workspace/ui/components/form";
+import { Input } from "@workspace/ui/components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+const formSchema = z.object({});
 export default function Page() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <div>
-          
-          <Button size="sm">Button</Button>
-
-        </div>
+        <Form {...form}>
+          <Input />
+        </Form>
+        <Button size="sm">Button</Button>
       </div>
     </div>
   );
